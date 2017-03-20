cd95344646fc9ffb8c3ebeab54deeaed22fc9b1f
Zhijia Yuan <Zhijia.Yuan@MotorolaSolutions.com>
2017-03-20 16:33:45 -0400

    init commit

---
+++ develop/yyyy-mm-dd-authoer_init-project_discription.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[5]:
+
+get_ipython().magic('version_information numpy, scipy, matplotlib, pandas')
+
+
+# In[ ]:
+
+
+
---
+++ src/example.py
@@ -0,0 +1,5 @@
+#!/bin/usr/env python
+
+# put reusable functions/classes/modules here so can be reused and shared 
+
+
